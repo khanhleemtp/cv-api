2478af5b67316df41c2899ad83110fb69286da75
khanhk62hust <khanhleemtp@gmail.com>
2021-11-04 05:34:58 +0700

    remove {}

--- server/app.js
+++ server/app.js
@@ -4,13 +4,11 @@ const path = require('path');
 // Start express app
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.get('/', (req, res) => {
   res.json({
