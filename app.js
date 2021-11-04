84669911ddfad8d37d9d243cffc8276d517c21fd
khanhk62hust <khanhleemtp@gmail.com>
2021-11-04 16:24:53 +0700

    Updae folder build

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.enable('trust proxy');
 app.options('*', cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
